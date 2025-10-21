368177cafeeb1b4452b058024684cba59cf046df
Melanie <taussatmelanie@gmail.com>
2025-10-21 16:47:54 +0200

    mise en forme de la page enfant de passeport, unfinished

--- AFPApp/PassportItems.swift
+++ AFPApp/PassportItems.swift
@@ -8,7 +8,11 @@
 import SwiftUI
 
 struct PassportItems: View {
+    
+    @State private var citySearchText = ""
+    
     var body: some View {
+        
         VStack{
             Text("Passeports")
                 .font(.title)
@@ -18,8 +22,37 @@ struct PassportItems: View {
                 .font(.subheadline)
                 .foregroundStyle(.greenSubtitle)
             
+            // SECTION VILLE
+            VStack(spacing: 15) {
+                
+                TextField("Chercher une ville", text: $citySearchText)
+                    .padding(10)
+                    .background(Color(.systemGray6))
+                    .cornerRadius(8)
+                    .padding(.horizontal)
+                    .autocorrectionDisabled(true)
+                    .textInputAutocapitalization(.never)
+            }
+        }
+        
+        HStack{
+            ZStack{
+                RoundedRectangle(cornerRadius: 8)
+                    .fill(Color.gray)
+                    .frame(width: 370, height: 110)
+                Image("TokyoMini")
+                    .resizable()
+                    .frame(width: 80, height: 80)
+                Text("4/12")
+                    .foregroundStyle(.white)
+                    .fontWeight(.bold)
+                
+            }
             
         }
+        
+        
+        
     }
 }
 
